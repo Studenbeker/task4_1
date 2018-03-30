ce4e67c938b4e710691cfdbdf85a75fdfc070576
Studenbeker <36299238+Studenbeker@users.noreply.github.com>
2018-03-30 15:25:35 +0300

    Update task4_1.sh

--- task4_1.sh
+++ task4_1.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
+exec 1> $PWD/task4_1.out
 
 BSN=(`dmidecode -s baseboard-serial-number`)
 if [ $BSN == 0 ];
 then
 BSN=Unkown
 fi
-
-slog=./task4_1.out
-
-function main
-
-{
+SSN=(`dmidecode -s system-serial-number`)
+if [ $SSN == 0 ];
+then
+SSN=Unkown
+fi
 
 echo "--- HARDWARE ---"
-echo "CPU "`cat /proc/cpuinfo | grep "model name" | sed -n 1p | grep -oP '(?=:).*'`
+echo "CPU"`cat /proc/cpuinfo | grep "model name" | sed -n 1p | grep -oP '(?=:).*'`
 echo "RAM: "`free | grep Mem | awk '{print $2}'` "KB"
 echo "Motherboard: "`dmidecode -s baseboard-manufacturer`" "`dmidecode -s baseboard-product-name`" "`dmidecode -s baseboard-version` $BSN
-echo "System serial number: "`dmidecode -s system-serial-number`
+echo "System serial number: "$SSN
 echo "--- SYSTEM ---"
 echo "OS Distribution: "`lsb_release -d --short`
 echo "Kernel version: "`uname -r`
 echo "Installation date: "`cat /var/log/dpkg.log | grep "install linux-firmware" | awk '{print $1}'`
 echo "Hostname: "`hostname`
 echo "Uptime: "`uptime -p | awk '{print $2,$3,$4,$5,$6,$7,$8}'`
-echo "Processes running: "`ps | sed -n 6p | awk '{print $1}'`
-echo "User logged in: " `who | wc -l`
+echo "Processes running: "`ps aux --no-headers | wc -l`
+echo "User logged in: "`who | wc -l`
 echo "--- NETWORK ---"
 
 NEWIF=(`ip address | awk '/mtu/{print $2}'`)
@@ -42,7 +42,3 @@ printf  "${NEWIF[$i]} ${NEWIP[$i]}\n"
 fi
 
 done
-
-}
-
-main 2>&1 | tee $slog
